212acd595a80ebf1481bfc94fa6ae249635307c0
Kévin Kowalsky <kevin.kowalsky1@gmail.com>
2022-09-01 15:09:46 +0200

    fix

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<EventType>
+ * @extends ServiceEntityRepository<Event>
  *
  * @method Event|null find($id, $lockMode = null, $lockVersion = null)
  * @method Event|null findOneBy(array $criteria, array $orderBy = null)
