2a3ac998674e973d93723ead9b320b7f05d949f1
Kévin Kowalsky <kevin.kowalsky1@gmail.com>
2022-09-01 12:02:35 +0200

    generate 10 random events

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Event;
+use DateTime;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        for ( $i=0; $i < 10; $i++) { 
+            $event = new Event();
+            $event
+                ->setName(implode(' ', $faker->words($faker->numberBetween(1, 3))))
+                ->setDescription($faker->text())
+                ->setPrice($faker->numberBetween(10000, 15000))
+                ->setCreatedAt($faker->dateTimeBetween('now', 'now'))
+                ->setReleasedAt($faker->dateTimeBetween('now', '+4 week'))
+                ->setImage((string) $faker->numberBetween(0, 3))
+            ;
+            $manager->persist($event);
+        }
+
+        $manager->flush();
+    }
+}
