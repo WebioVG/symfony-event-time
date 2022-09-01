f813070f595ae245048440d63d3122fa607482bc
Kévin Kowalsky <kevin.kowalsky1@gmail.com>
2022-09-01 12:20:17 +0200

    added finished_at column

--- migrations/Version20220901101622.php
+++ migrations/Version20220901101622.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220901094238 extends AbstractMigration
+final class Version20220901101622 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220901094238 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, price INT DEFAULT NULL, created_at DATETIME NOT NULL, released_at DATETIME NOT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE event ADD finished_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE event');
+        $this->addSql('ALTER TABLE event DROP finished_at');
     }
 }
